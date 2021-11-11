c36926e02d33069bc7a2650d40c49b2931823dcd
unknown <nolan.beihl@gmail.com>
2021-11-11 08:19:34 -0700

    Removed notes and unused code throughout app.js

--- most-wanted-starter-code-master/app.js
+++ most-wanted-starter-code-master/app.js
@@ -1,12 +1,5 @@
 "use strict"
 
-
-//Menu functions.
-//Used for the overall flow of the application.
-/////////////////////////////////////////////////////////////////
-//#region 
-
-// app is the function called to start the entire application
 function app(people){
   let searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
   let searchResults;
@@ -15,7 +8,7 @@ function app(people){
       searchResults = searchByName(people);
       if(searchResults.length === 0){
         alert("Could not find that individual.");
-        return app(people); // restart
+        return app(people);
       }
         mainMenu(searchResults, people);
       break;
@@ -29,29 +22,15 @@ function app(people){
       }
       break;
       default:
-    app(people); // restart app
+    app(people); 
       break;
-  }
-  
-  // Call the mainMenu function ONLY after you find the SINGLE person you are looking for
-  // mainMenu(searchResults, people);
+  }  
 }
 
-// Menu function to call once you find who you are looking for
 function mainMenu(person, people){
-
-  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
-
-  // if(!person){
-  //   alert("Could not find that individual.");
-  //   return app(people); // restart
-  // }
-
   let displayOption = promptFor("Found " + person[0].firstName + " " + person[0].lastName + " . Do you want to know their 'info', 'family', 'parents', or 'descendants'? Type the option you want or 'restart' or 'quit'", autoValid);
-
   switch(displayOption){
     case "info":
-    // TODO: get person's info
     alert(`          First Name: ${person[0].firstName}  \n 
            Last Name: ${person[0].lastName} \n
            Gender: ${person[0].gender} \n 
@@ -62,7 +41,6 @@ function mainMenu(person, people){
            Occupation: ${person[0].occupation}`)
     break;
     case "family":
-    // TODO: get persons family
     var spouse = searchForSpouse(people, person[0].currentSpouse)
     var descendants = searchForDescendants(people, person[0].id)
     var parents = searchForParents(people, person[0].parents)
@@ -90,18 +68,17 @@ function mainMenu(person, people){
     }
     break;
     case "restart":
-    app(people); // restart
+    app(people);
     break;
     case "quit":
-    return; // stop execution
+    return;
     default:
-    return mainMenu(person, people); // ask again
+    return mainMenu(person, people);
   }
 }
 
 function attributeMenu(people){
   let displayOption = promptFor("What attribute would you like to search for?: 'Gender', 'Date of Birth', 'Height', 'Weight', 'Eye Color' ", autoValid).toLowerCase()
-
   switch (displayOption){
     case "gender":
     var genderInput = prompt("Which gender are you lookin for? Male or Female?: ").toLowerCase()
@@ -183,14 +160,7 @@ function multipleAttributesMenu(people){
     })
     alert(filteredNames.join("\n"))
 }
-//#endregion
 
-//Filter functions.
-//Ideally you will have a function for each trait.
-/////////////////////////////////////////////////////////////////
-//#region 
-
-//nearly finished function used to search through an array of people to find matching first and last name and return a SINGLE person object.
 function searchByName(people){
   let firstName = promptFor("What is the person's first name?", autoValid).toLowerCase();
   let lastName = promptFor("What is the person's last name?", autoValid).toLowerCase();
@@ -203,11 +173,9 @@ function searchByName(people){
       return false;
     }
   })
-  // TODO: find the person single person object using the name they entered.
   return foundPerson;
 }
 
-//unfinished function to search through an array of people to find matching eye colors. Use searchByName as reference.
 function searchByAttribute(people, attribute, searchAttribute){
   let attributeList = [];
   people.forEach(person =>{
@@ -240,7 +208,6 @@ function searchAllAttributes(people, attList) {
     }
   return filteredPeople
 }
-
 
 function searchForSpouse(people, spouse){
   var foundSpouse = '';
@@ -278,42 +245,6 @@ function searchForDescendants(people, parentId) {
   return children
 }
 
-//#endregion
-
-//Display functions.
-//Functions for user interface.
-/////////////////////////////////////////////////////////////////
-//#region 
-
-// alerts a list of people
-function displayPeople(people){
-  alert(people.map(function(person){
-    return person.firstName + " " + person.lastName;
-  }).join("\n"));
-}
-
-function displayPerson(person){
-  // print all of the information about a person:
-  // height, weight, age, name, occupation, eye color.
-  let personInfo = "First Name: " + person.firstName + "\n";
-  personInfo += "Last Name: " + person.lastName + "\n";
-  // TODO: finish getting the rest of the information to display.
-  alert(personInfo);
-}
-
-//#endregion
-
-
-
-//Validation functions.
-//Functions to validate user input.
-/////////////////////////////////////////////////////////////////
-//#region 
-
-//a function that takes in a question to prompt, and a callback function to validate the user input.
-//response: Will capture the user input.
-//isValid: Will capture the return of the validation function callback. true(the user input is valid)/false(the user input was not valid).
-//this function will continue to loop until the user enters something that is not an empty string("") or is considered valid based off the callback function(valid).
 function promptFor(question, valid){
   let isValid;
   do{
@@ -323,7 +254,6 @@ function promptFor(question, valid){
   return response;
 }
 
-// helper function/callback to pass into promptFor to validate yes/no answers.
 function yesNo(input){
   if(input.toLowerCase() == "yes" || input.toLowerCase() == "no"){
     return true;
@@ -333,16 +263,6 @@ function yesNo(input){
   }
 }
 
-// helper function to pass in as default promptFor validation.
-//this will always return true for all inputs.
 function autoValid(input){
-  return true; // default validation only
+  return true; 
 }
-
-//Unfinished validation function you can use for any of your custom validation callbacks.
-//can be used for things like eye color validation for example.
-function customValidation(input){
-  
-}
-
-//#endregion
